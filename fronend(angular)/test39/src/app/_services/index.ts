bb2cfe18bb9a780717796fa7ac389f99a45b9e6f
sdev3millions <42561220+sdev3millions@users.noreply.github.com>
2019-03-09 18:10:21 -0800

    asdda

--- fronend(angular)/test39/src/app/_services/index.ts
+++ fronend(angular)/test39/src/app/_services/index.ts
@@ -1,5 +1,4 @@
 ﻿export * from './alert.service';
 export * from './authentication.service';
 export * from './user.service';
-export * from './router.animations';
 export * from './school.service';
--- fronend(angular)/test39/src/app/_services/user.service.ts
+++ fronend(angular)/test39/src/app/_services/user.service.ts
@@ -30,4 +30,19 @@ export class UserService {
     getAll() {
         return this.http.get<User[]>(`${environment.baseUrl}/users`);
     }
+    getById(id: number) {
+        return this.http.get(`${environment.baseUrl}/users/` + id);
+    }
+
+    register(user: User) {
+        return this.http.post(`${environment.baseUrl}/users/register`, user);
+    }
+
+    update(user: User) {
+        return this.http.put(`${environment.baseUrl}/users/` + user.id, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${environment.baseUrl}/users/` + id);
+    }
 }
