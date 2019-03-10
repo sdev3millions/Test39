379b1151b9edc1d142a174476847cacaf2282295
sdev3millions <42561220+sdev3millions@users.noreply.github.com>
2019-03-09 17:53:55 -0800

    front end-logn, signup, jwt authentication add

--- fronend(angular)/test39/src/app/app-routing.module.ts
+++ fronend(angular)/test39/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomepageComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
