ba34d527c5cdd7518d67368b9b2545c2e1680348
sdev3millions <42561220+sdev3millions@users.noreply.github.com>
2019-03-09 18:32:06 -0800

    front end

--- fronend(angular)/test39/src/app/dashboard/dashboard.component.ts
+++ fronend(angular)/test39/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../_models';
+import { UserService } from '../_services';
+import { first } from 'rxjs/operators';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+  users: User[] = [];
+  constructor(private userService: UserService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
+  }
   ngOnInit() {
+    this.loadAllUsers();
+  }
+  private loadAllUsers() {
+    this.userService.getAll().pipe(first()).subscribe(users => {
+      this.users = users;
+      console.log(this.users);
+    });
   }
 
 }
--- fronend(angular)/test39/src/environments/environment.ts
+++ fronend(angular)/test39/src/environments/environment.ts
@@ -8,10 +8,3 @@ export const environment = {
   baseUrl: 'http://localhost:3000'
 };
 
-/*
- * In development mode, for easier debugging, you can ignore zone related error
- * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
- * because it will have a performance impact when errors are thrown
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
