7c66cc218a9fede32168d1e547e10daad63748d4
sdev3millions <42561220+sdev3millions@users.noreply.github.com>
2019-03-09 17:19:06 -0800

    backend

---
+++ backend(node)/server.js
@@ -0,0 +1,68 @@
+﻿require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+
+////
+var multer = require('multer');
+app.use(function (req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
+// app.use(express.static('./uploads'));
+////
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+// app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+app.use('/schools', require('./schools/schools.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+////////
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, '../client/src/assets/images/');
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        var imageName = file.originalname;
+        // var imageName = file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1];
+        cb(null, imageName);
+    }
+});
+
+var upload = multer({ //multer settings
+    storage: storage
+}).single('file');
+
+/** API path that will upload the files */
+app.post('/upload', function (req, res) {
+    upload(req, res, function (err) {
+        console.log(req.file);
+        if (err) {
+            res.json({ error_code: 1, err_desc: err });
+            return;
+        }
+        res.json({ error_code: 0, err_desc: null });
+    });
+});
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? 80 : 3000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
---
+++ fronend(angular)/test39/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
